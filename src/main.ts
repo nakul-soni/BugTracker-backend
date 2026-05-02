5fd9cde8fd00d289a4397c36f8c06534341be25f
nakul-soni <nakulsoni2006@gmail.com>
2026-05-02 06:33:02 -0600

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// Trigger reload
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
